59328417f1cd75fb637dc9c7fa5215689644d5df
Gungun Septian <itsmegoens@gmail.com>
2013-10-10 15:58:28 +0700

    test2

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 /*
  *---------------------------------------------------------------
- * APPLICATION ENVIRONMENT
+ * APPLICATION ENVIRONMENT NEXT GUNGUn
  *---------------------------------------------------------------
  *
  * You can load different configurations depending on your
